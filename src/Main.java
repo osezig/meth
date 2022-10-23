d045e7392a2954fee33abc12fd45f72baa6caead
sergey_iagfarov <dedaz27@mail.ru>
2022-10-23 23:45:17 +0500

    Методы

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
+import java.time.LocalDate;
+
 public class Main {
-
-    public static void printIsLeapYear(int year) {
-        boolean yearIsLeap = isLeap(year);
-        printIsLeapYearResult(year, yearIsLeap);
-    }
-
-    private static boolean isLeap(int year) {
-        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    }
-    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
-        if (yearIsLeap) {
-            System.out.println(year + " високосный");
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceOld = isDeviceOld(deviceYear);
+        System.out.print("Установите ");
+        if (deviceOld) {
+            System.out.print("lite ");
+        }
+        System.out.println("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
         } else {
-            System.out.println(year + "не является високосным");
+            System.out.println("Android");
         }
     }
-
-    public static void main(String[]args) {
-        System.out.println("Методы. Задача 1");
-        int year = 2020;
-        printIsLeapYear(year);
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
+    }
+    public static void main(String[] args) {
+        System.out.println("Методы. Задача 2");
+        int deviceYear = 2020;
+        int clientOS = 0;
+        recommendApplicationVersion(clientOS, deviceYear);
     }
 }
+
+
+
 
 
